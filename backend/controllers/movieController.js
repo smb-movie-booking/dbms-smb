d61e63f58c9ec17d97d59d6277448f68cf43818e
ahammedameen <ameenmoyikkal123@gmail.com>
2025-08-17 13:24:50 +0530

    Add API Requirement for Movie Listing and Filtering

--- backend/controllers/movieController.js
+++ backend/controllers/movieController.js
@@ -1,3 +1,34 @@
 const movieModel = require('../models/movieModel');
 
+const handleExplore = async(req,res) => {
+    try{
+        const {city, language, genre, format, theater, showDate, movie } = req.query;
+        if(movie) {
+            const details = await movieModel.getMovieDetail(movie);
+            return res.json(details);
+        }
+        if (theater && showDate) {
+        const movies = await movieModel.getMoviesByTheaterAndDate(theater, showDate);
+        const shows = await showModel.fetchShowsByTheaterAndDate(theater, showDate);
+        const merged = movies.map(m => ({
+            ...m,
+            shows: shows.filter(s => s.movieId === m.movieId)
+        }));
+
+        return res.json(merged);
+        }
+        if (city) {
+        const movies = await movieModel.getMoviesByCity(city, { language, genre, format });
+        return res.json(movies);
+        }
+        return res.status(400).json({ error: "Invalid query params" });
+
+    }catch(err){
+        console.error(err);
+        res.status(500).json({error: "server error"});
+    }
+}
+
+export default { handleExplore };
+
 
