12ddf69348bf5119510c00c2f293f140182dac9c
aadithkichu <aadithkichu123@gmail.com>
2025-10-18 14:30:11 +0530

    comit

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-
+console.log("REACT_APP_API_URL:", process.env.REACT_APP_API_URL);
 export const axiosInstance=axios.create({
     baseURL:process.env.REACT_APP_API_URL,
     withCredentials:true,
