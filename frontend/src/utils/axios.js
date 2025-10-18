f594413d3de24f91663dbe97463b57bb28a4a129
aadithkichu <aadithkichu123@gmail.com>
2025-10-18 14:45:45 +0530

    change

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-console.log("VITE_API_URL:", process.env.VITE_API_URL);
+console.log("VITE_API_URL:", import.meta.env.VITE_API_URL);
 export const axiosInstance=axios.create({
-    baseURL:process.env.VITE_API_URL,
+    baseURL:import.meta.env.VITE_API_URL,
     withCredentials:true,
 })
